997c8f2152f0eac9b43d9cf73b7e14f35a9e58db
marco pereira <viniciopq@hotmail.com>
2019-09-07 22:21:44 -0600

    trouble shooting db uri

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
+const DB_URI = require('../../keys/keys')
 
-const DB_URI = process.env.DATABASE_URI
+//const DB_URI = process.env.DATABASE_URI
 
-mongoose.connect(`${DB_URI}`, {
+mongoose.connect("", {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-.catch(e =>  console.log(e))
