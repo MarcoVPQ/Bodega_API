f4a92b901b0e6b07cf035dbcf8da35018a3f7f30
marco pereira <viniciopq@hotmail.com>
2019-09-07 22:28:53 -0600

    nothing

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const DB_URI = require('../../keys/keys')
+ 
 
 //const DB_URI = process.env.DATABASE_URI
 
-mongoose.connect(DB_URI, {
+mongoose.connect( process.env.DATABASE_URL , {
     useNewUrlParser: true,
     useCreateIndex: true
 })
