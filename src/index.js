7ded46335861bf01887dbdc230baa8d7b19ee26d
marco pereira <viniciopq@hotmail.com>
2019-09-13 08:41:11 -0600

    added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 require('./db/db')
 
 const productRouter = require('./routes/productRoutes')
 const app = express()
 
-
+app.use(cors());
 app.use(express.json())
 app.use(productRouter)
 
